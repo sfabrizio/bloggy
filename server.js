b9c8f16733080813743625414f422390587a9ccd
Samuel Fabrizio <samuel.fabrizio@sovanta.com>
2016-04-10 21:49:23 -0300

    [ADD] Basic server file

---
+++ server.js
@@ -0,0 +1,27 @@
+var http = require('http');
+var express = require('express');
+var routes = require('./routes');
+
+
+var app = express();
+
+// routes
+app.get(  '/',               routes.index );
+app.get(  '/api/getAll',      routes.getAll );
+app.post( '/api/create',      routes.create );
+app.get(  '/api/destroy/:id', routes.destroy );
+app.get(  '/api/edit/:id',    routes.edit );
+
+var server = http.createServer(app);
+var port = app.get('port') || 3000;
+
+function start() {
+    server.listen(port, function () {
+        console.log('Express server listening on port: ' + port);
+    });
+}
+
+exports.start = start;
+exports.app = app;
+
+start(); //automatically start form now.
