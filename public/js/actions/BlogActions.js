6e4e635ba6c974c6f47d358e5335dec6999772d4
Samuel Fabrizio <samuel.fabrizio@sovanta.com>
2016-04-15 00:47:24 -0300

    [ADD] create/delete actions

--- public/js/actions/BlogActions.js
+++ public/js/actions/BlogActions.js
@@ -2,26 +2,35 @@ import dispatcher from "../dispatcher";
 import baseURL from "../config";
 import axios from "axios";
 
-export function createBlog(title, content) {
-    dispatcher.dispatch({
-        type: "CREATE_BLOG",
-        title,
-        content
+export function createBlog(data) {
+
+    axios.post(`${baseURL}/api/create`, {data}).then( (data) => {
+        dispatcher.dispatch({
+            type: "CREATE_BLOG",
+            data
+        });
     });
 }
 
 export function deleteBlog(id) {
-    dispatcher.dispatch({
-        type: "DELETE_BLOG",
-        id,
+
+    axios(`${baseURL}/api/remove`, {id}).then( (data) => {
+        dispatcher.dispatch({
+            type: "DELETE_BLOG",
+            id
+        });
     });
+
 }
 
 export function reloadBlogs() {
 
     axios(`${baseURL}/api/getAll`).then( (data) => {
         const parsedData  = sortByKey(data.data.blog, 'id');
-        dispatcher.dispatch({type: "RECEIVE_BLOGS", blog: parsedData});
+        dispatcher.dispatch({
+            type: "RECEIVE_BLOGS",
+            blog: parsedData
+        });
     });
 
     dispatcher.dispatch({type: "FETCH_BLOGS"});
