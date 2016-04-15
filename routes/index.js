5d781b28c510e5b26b8de792bc89bd627e1321a7
Samuel Fabrizio <samuel.fabrizio@sovanta.com>
2016-04-15 13:02:11 -0300

    [ADD] use handlers on router main file

--- routes/index.js
+++ routes/index.js
@@ -1,66 +1,14 @@
-var fs = require('fs');
-var jsonPath = './spec/data.json';
-var routes = {};
-
-routes.index = function ( req, res, next ){
-    res.send('Sam\'s blog');
-};
-
-routes.getAll = function ( req, res, next ){
-    fs.readFile(jsonPath, 'utf8', function (err, data) {
-        if (err) throw err;
-        var obj = JSON.parse(data);
-        res.json(obj);
-    });
-};
-
-routes.create = function ( req, res ){
-    var data, jsonData, newObj;
-
-    req.on('data', function(chunk) {
-        data = JSON.parse(chunk.toString()).data;
-        //very simple validation
-        if (!data || !data.title || !data.content){
-            res.status(400).send({ error: 'Something failed!' });
-        }
-    });
-    //let's read the json data file
-    fs.readFile(jsonPath, 'utf8', function (err, file) {
-        if (err) throw err;
-        jsonData = JSON.parse(file);
-        newObj = {
-            "id": Date.now(),
-            "title": data.title,
-            "content": data.content
-        };
-        jsonData.blog.push(newObj);
-        //okey got it! let's write into the "DB" XD
-        fs.writeFile(jsonPath, JSON.stringify(jsonData), function (err) {
-            if (err){
-                console.log(err);
-            }
-        });
-        res.json(newObj).end();
-    });
-};
-
-routes.update = function ( req, res, next ){
-    res.redirect( '/' );
-};
-
-routes.remove = function ( req, res, next ){
-    res.redirect( '/' );
-};
+var handlers = require('./handlers');
 
 module.exports = function(app) { //routes setup
-    app.get(  '/',                routes.index );
-    app.get(  '/api/getAll',      routes.getAll );
-    app.post( '/api/create',      routes.create );
-    app.get(  '/api/remove/:id',  routes.remove );
-    app.put(  '/api/update/:id',  routes.update );
-    // app.get('*', function(req, res) { //Route not found -- Set 404
-    //     res.json({
-    //         'route': 'Sorry this page does not exist.'
-    //     });
-    // });
+    app.get(    '/',                handlers.index );
+    app.get(    '/api/getAll',      handlers.getAll );
+    app.post(   '/api/create',      handlers.create );
+    app.delete( '/api/remove/:id',  handlers.remove );
+    app.put(    '/api/update/:id',  handlers.update );
+    app.get('*', function(req, res) { //Route not found -- Set 404
+        res.json({
+            'route': 'Sorry this page does not exist.'
+        });
+    });
 };
