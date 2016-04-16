64b2bc811c6b7e373ed642c1a8bad57be26a70ae
Samuel Fabrizio <samuel.fabrizio@sovanta.com>
2016-04-16 17:50:25 -0300

    [ADD] simple footer component

--- public/js/components/Layout.js
+++ public/js/components/Layout.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Header from "./Header/Header";
 import Body from "./Body/Body";
+import Footer from "./Footer/Footer";
 
 export default class Layout extends React.Component {
     constructor() {
@@ -19,7 +20,9 @@ export default class Layout extends React.Component {
             <div>
                 <Header />
                 <Body />
+                <Footer />
             </div>
+
         );
     }
 }
