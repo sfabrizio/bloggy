7117083fa1c792edd03bbacd87c85b2788576155
Samuel Fabrizio <samuelfabrizio@gmail.com>
2016-04-18 02:40:00 -0300

    [MOD] improve validation on update handlrer

--- routes/handlers.js
+++ routes/handlers.js
@@ -51,33 +51,53 @@ handlers.create = function ( req, res ){
 };
 
 handlers.update = function ( req, res ){
-    var jsonData = {},
-        updateData;
+    var updateData = false;
     req.on('data', function(chunk) {
-        updateData = JSON.parse(chunk.toString()).data;
+        var data = JSON.parse(chunk.toString());
+        validateData(data);
+    });
+
+    function validateData(data){
         //very simple validation
+        if (!data){
+            res.status(400).send({ error: 'Something failed!' });
+        }
+        updateData = data.data;
+        //validate a litte betrer
         if (!updateData || !updateData.id || !updateData.title || !updateData.content){
             res.status(400).send({ error: 'Something failed!' });
         }
-
+        //ok let's read the file
         fs.readFile(jsonPath, 'utf8', function (err, readData) {
             if (err) throw err;
-            jsonData = JSON.parse(readData);
+            var jsonData = JSON.parse(readData);
+            //keep it simple remove old first
             removeOldData(updateData.id, jsonData);
         });
-    });
+    }
 
     function removeOldData(id, jsonData){
+        if (!id || !jsonData){
+            res.status(400).send({ error: 'Something failed!' });
+        }
         utils.findAndRemoveFromJsonArray(jsonData.blog, 'id', id);
         updateJson(jsonData);
     }
 
     function updateJson(jsonData){
+        if (!jsonData){
+            res.status(400).send({ error: 'Something failed!' });
+        }
+        //update json
         jsonData.blog.push(updateData);
         writeNewJson(jsonData);
     }
 
     function writeNewJson(jsonData){
+        if (!jsonData){
+            res.status(400).send({ error: 'Something failed!' });
+        }
+        //keep it simple  now write new data
         fs.writeFile(jsonPath, JSON.stringify(jsonData), function (err) {
             if (err){
                 console.log(err);
